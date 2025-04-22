995e66420f41020a4547010192d044becc1b46e0
AkamediasTech <technique@akamedias.fr>
2025-04-22 17:44:27 +0200

    Pac avec form mais bug affichage multiple

---
+++ pacForm/form.js
@@ -0,0 +1,342 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const nextBtn = document.getElementById('nextBtn');
+    const form = document.getElementById('form');
+    let currentStep = 0;
+    const inputs = form.querySelectorAll('.input-container');
+    const answers = {}; // Stocke les réponses des étapes avec des boutons
+    const containerCricle = document.querySelector('.containerCricle');
+    let loaderCounter = 0;
+    let buttonTextCounter = 0;
+    let displayStepCounter = 1;
+    let ville = "";
+
+    const buttonTextTab = [
+        "",
+        "Suivant",
+        "Obtenir mon estimation",
+    ];
+    
+
+    // Création du message de validation (caché au départ)
+    const successMessage = document.createElement('div');
+    successMessage.classList.add('success-message');
+    successMessage.innerHTML = `
+        <div class="checkmark-animation">
+            <img src="assets/validate.svg" alt="Validation" class="checkmark">
+        </div>
+        <p>Votre formulaire a été soumis avec succès !</p>
+    `;
+    successMessage.style.display = 'none';
+    form.appendChild(successMessage);
+
+    const errorMessage = document.createElement('div');
+    errorMessage.classList.add('error-message');
+    errorMessage.innerHTML = `
+        <div class="checkmark-animation">
+            <img src="assets/error.svg" alt="error" class="checkmark">
+        </div>
+        <p>Erreur lors de l'envoi du formulaire</p>
+    `;
+    errorMessage.style.display = 'none';
+    form.appendChild(errorMessage);
+
+    // Gestion des boutons cliquables
+    const optionButtons = form.querySelectorAll('.option-btn');
+    optionButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            displayStep();
+            buttonText();
+            const value = button.dataset.value;
+            const step = button.dataset.step;
+            answers[`step${step}`] = value;
+            inputs[currentStep].classList.remove('active');
+            currentStep++;
+            console.log("count:", currentStep);
+            if (currentStep === 5){
+                showLoaderAndNextStep();
+            }else if (currentStep < inputs.length) {
+                inputs[currentStep].classList.add('active');
+                toggleNextButtonVisibility();
+            } else {
+                showLoader();
+                sendFormData();
+            }
+        });
+    });
+
+    // Gestion du bouton "Suivant"
+    nextBtn.addEventListener('click', () => {
+        if (validateCurrentStep()) {
+            const isLastStep = currentStep >= inputs.length - 1;
+            const shouldShowLoader = !(currentStep === 5 || currentStep === 6);
+            displayStep();
+            buttonText();
+
+            console.log("Étape actuelle:", currentStep);
+    
+            let delayDiv = 0; // Valeur par défaut
+            if (shouldShowLoader) {
+                showLoader();
+                const delay = randomDelay();
+                delayDiv = delay;
+    
+                setTimeout(() => {
+                    nextText();
+                    passToSucess(); 
+                }, delay / 2);
+            }
+    
+            setTimeout(() => {
+                if (!isLastStep) {
+                    inputs[currentStep].classList.remove('active');
+                    currentStep++;
+                    inputs[currentStep].classList.add('active');
+                    toggleNextButtonVisibility();
+                } else {
+                    showLoader();
+                    sendFormData();
+                }
+                if (shouldShowLoader) {
+                    hideLoader();
+                }
+            }, delayDiv);
+        }
+    });
+    
+
+    function showLoaderAndNextStep() {
+        showLoader();
+        const delay = randomDelay(); 
+        setTimeout(() => {
+            nextText();
+            passToSucess(); 
+        }, delay / 2);
+        setTimeout(() => {
+            hideLoader(); 
+            if (currentStep < inputs.length - 1) {
+                inputs[currentStep].classList.add('active');
+                toggleNextButtonVisibility();
+            } else {
+                sendFormData();
+                console.log(delay,"ms");
+            }
+        }, delay);
+    }   
+
+    function showLoader() {
+        const loaderContainer = document.getElementById('loader-container'); 
+        loaderContainer.style.display = 'flex';
+        // document.querySelector("form").style.display = "none";
+        document.querySelector("form").style.visibility = "hidden";
+        nextText();
+        document.body.style.overflow = "hidden";
+
+        setTimeout(() => {
+            loaderContainer.style.opacity = '1';
+        }, 50);
+    }
+
+    function hideLoader() {
+        const loaderContainer = document.getElementById('loader-container'); 
+        loaderContainer.style.opacity = '0';
+        // document.querySelector("form").style.display = "flex";
+        document.querySelector("form").style.visibility = "visible";
+        nextText();
+        passToSucessNone();
+
+        setTimeout(() => {
+            loaderContainer.style.display = 'none';
+            document.body.style.overflow = "auto";
+        }, 200);
+    }
+
+    function passToSucess() {
+        const loaderContainer = document.getElementById('loader-container');
+        const okCircleFilled = document.getElementById('ok-circle-filled'); 
+        loaderContainer.style.opacity = '0';
+        loaderContainer.style.display = 'none';
+        setTimeout(() =>{
+            okCircleFilled.style.opacity = '1';
+            okCircleFilled.style.display = 'flex';
+            okCircleFilled.style.width = '100px';
+            okCircleFilled.style.height = '100px';
+
+        }, 200);
+        
+    }
+
+    function passToSucessNone() {
+        const okCircleFilled = document.getElementById('ok-circle-filled'); 
+        okCircleFilled.style.opacity = '0';
+        okCircleFilled.style.display = 'none';
+        okCircleFilled.style.width = '0px';
+        okCircleFilled.style.height = '0px';
+    }
+
+    function nextText(){
+        const loaderMessages = [
+            "",
+            "Recherche de vos aides régionales",
+            `Bonne nouvelle ! ${ville} est éligible`, // Utilisation correcte des backticks
+            "",
+            "Calcul des aides MaPrimeRénov",
+            "Votre logement est éligible à MaPrimeRénov’",
+            "",
+            "Envoi du formulaire en cours",
+            "",
+            ""
+        ];
+        
+        const loaderText = document.getElementById('loader-text');
+        loaderCounter ++;
+        loaderText.textContent = loaderMessages[loaderCounter];
+    }
+
+    function buttonText(){
+        const currentStepSync = currentStep + 1;
+        if(currentStepSync === 0 || currentStepSync >= 4){
+            buttonTextCounter ++;
+            const buttonText = document.getElementById('nextBtn');
+            buttonText.textContent = buttonTextTab[buttonTextCounter -1];
+        }else{
+            return
+        }
+    }
+
+    function displayStep(){
+        const currentStepSync = currentStep + 1;
+        if(currentStepSync === 1 || currentStepSync === 5 || currentStepSync === 6){
+            document.getElementById(`circle-${displayStepCounter}`)?.classList.remove('activeCircle');
+            displayStepCounter++;
+            document.getElementById(`circle-${displayStepCounter}`)?.classList.add('activeCircle');
+        }else{
+            return
+        }
+
+    }
+
+    function validateCurrentStep() {
+        const currentInput = inputs[currentStep];
+        const inputsInStep = currentInput.querySelectorAll('input');
+        let isValid = true;
+
+        const errorMessages = currentInput.querySelectorAll('.error-message');
+        errorMessages.forEach(msg => msg.remove());
+
+        for (let input of inputsInStep) {
+            if (input.required && !input.value.trim()) {
+                showError(input, 'Ce champ est requis.');
+                isValid = false;
+            }
+
+            if (input.id === 'codePostal' && !/^\d{5}$/.test(input.value.trim())) {
+                showError(input, 'Le code postal doit être composé de 5 chiffres.');
+                isValid = false;
+            }
+
+            if (input.type === 'email' && input.value && !input.checkValidity()) {
+                showError(input, 'Veuillez entrer une adresse email valide.');
+                isValid = false;
+            }
+
+            if (input.type === 'tel' && input.value && !input.checkValidity()) {
+                showError(input, 'Veuillez entrer un numéro de téléphone valide.');
+                isValid = false;
+            }
+            if (currentStep === 0) {
+                ville = input.value;
+                console.log("Ville enregistrée :", ville);
+            }
+            
+        }
+        return isValid;
+    }
+
+    function showError(input, message) {
+        console.log(`Ajout d'une erreur pour ${input.name}: ${message}`);
+    
+        // Stocker l'ancien placeholder si ce n'est pas déjà fait
+        if (!input.dataset.placeholder) {
+            input.dataset.placeholder = input.placeholder;
+        }
+    
+        // Modifier le placeholder pour afficher l'erreur
+        input.placeholder = message;
+        input.classList.add('error-input'); // Ajouter une classe pour le style
+    
+        // Vider la valeur pour forcer l'affichage du placeholder
+        input.value = '';
+    
+        // Ajouter un écouteur pour restaurer le placeholder initial
+        input.addEventListener('input', function restorePlaceholder() {
+            input.placeholder = input.dataset.placeholder;
+            input.classList.remove('error-input');
+            input.removeEventListener('input', restorePlaceholder);
+        });
+    }
+
+    function toggleNextButtonVisibility() {
+        const isSelectionStep = inputs[currentStep].classList.contains('selection-step');
+        nextBtn.style.display = isSelectionStep ? 'none' : 'block';
+    }
+
+    function sendFormData() {
+        const formData = new FormData(form);
+        Object.keys(answers).forEach(key => {
+            formData.append(key, answers[key]);
+        });
+
+        fetch('https://techaka.app.n8n.cloud/webhook/8ed7e6eb-317b-42cf-9826-68b1680efa0d', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Données envoyées:', data);
+            finalizeForm();
+        })
+        .catch(error => {
+            console.error('Erreur:', error);
+            finalizeFormError();
+        });
+    }
+
+    function finalizeForm() {
+        setTimeout(() => {
+            hideLoader(); 
+            containerCricle.style.opacity = '0';
+    
+            inputs.forEach(input => input.style.opacity = '0');
+            inputs.forEach(input => input.style.display = 'none');
+            nextBtn.style.display = 'none';
+            containerCricle.style.display = 'none';
+            successMessage.style.display = 'flex';
+            successMessage.style.opacity = '1';
+            console.log(answers);
+            if (answers.step1 !== "Appartement" && answers.step2 !== "Locataire") {
+                console.log("validation");
+                // fbq('track', 'Lead');
+            }
+        }, randomDelay());
+    }
+    
+    function finalizeFormError() {
+        setTimeout(() => {
+            hideLoader(); 
+            containerCricle.style.opacity = '0';
+    
+            inputs.forEach(input => input.style.opacity = '0');
+            inputs.forEach(input => input.style.display = 'none');
+            nextBtn.style.display = 'none';
+            containerCricle.style.display = 'none';
+            errorMessage.style.display = 'flex';
+            errorMessage.style.opacity = '1';
+        }, randomDelay());
+    }
+    
+
+    function randomDelay() {
+        return Math.random() * (7500 - 6000) + 6000;
+    }
+    toggleNextButtonVisibility();
+});
