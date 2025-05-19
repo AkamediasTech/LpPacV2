22866d941f88fef94d84468815623de2aa9e9a8b
AkamediasTech <technique@akamedias.fr>
2025-05-19 11:04:27 +0200

    ajout page validation ite et pac

---
+++ validation/validationPac/script.js
@@ -0,0 +1,35 @@
+let index = 0;
+const images = document.querySelectorAll(".carousel-images img");
+const totalImages = images.length;
+const visibleImages = 3;
+const dots = document.querySelectorAll(".dot");
+
+function updateCarousel() {
+    const offset = -index * 316;
+    document.getElementById("carouselImages").style.transform = `translateX(${offset}px)`;
+    updateDots();
+}
+
+function nextSlide() {
+    if (index < totalImages - visibleImages) {
+        index++;
+    } else {
+        index = 0;
+    }
+    updateCarousel();
+}
+
+function prevSlide() {
+    if (index > 0) {
+        index--;
+    } else {
+        index = totalImages - visibleImages;
+    }
+    updateCarousel();
+}
+
+function updateDots() {
+    const activeDot = document.querySelector(".dot.focusDot");
+    if (activeDot) activeDot.classList.remove("focusDot");
+    dots[index % dots.length].classList.add("focusDot");
+}
